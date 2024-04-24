2412be4e48e7341a2abefdb07266b3871682a520
brooklinbecker <149913166+brooklinbecker@users.noreply.github.com>
2024-04-24 19:51:28 -0400

    model file

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,37 +1,50 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Modelling the relationship between Anger and Social Aversion using linear regression
+# Author: Brooklin Becker
+# Date: 24 April 2024 
+# Contact: brooklin.becker@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
 library(tidyverse)
-library(rstanarm)
-
-#### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+library(knitr)
+library(kableExtra)
+library(ggplot2)
+library(dbplyr)
+library(janitor)
+library(openxlsx)
+library(reshape2)
+library(tidyr)
+library(arrow)
 
 ### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+# Define the data for Anger and Social Aversion, which is taken from Table 6 in the paper
+anger_social_aversion <- data.frame(
+  Anger = c(1.31, 1.37, 1.30, 1.34, 1.32, 1.40, 1.20, 1.22),
+  Social_Aversion = c(15.25, 14.21, 14.62, 14.10, 14.58, 14.15, 10.93, 9.68)
+)
 
+# Perform linear regression
+lm_model <- lm(Social_Aversion ~ Anger, data = anger_social_aversion)
+
+# Plotting the data points and line of best fit
+ggplot(data = anger_social_aversion, aes(x = Anger, y = Social_Aversion)) +
+  geom_point() +
+  labs(title = "Linear Relationship Between Anger and Social Aversion",
+       x = "Anger",
+       y = "Social Aversion") +
+  # Add the line of best fit
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  # Add the equation for the line of best fit
+  geom_text(aes(x = max(anger_social_aversion$Anger), y = max(anger_social_aversion$Social_Aversion),
+                label = paste("y =", round(lm_model$coefficients[2], 2), "x", round(lm_model$coefficients[1], 2))),
+            color = "black", vjust = 1.5, hjust = 1)
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+  anger_social_aversion,
+  file = "outputs/models/first_model.rds"
 )
 
 
